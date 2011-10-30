ffe34b0620390ae298ae1a82c9aedd3a9098d5c5
suer <sue@nagoya-u.jp>
2011-10-30 20:10:38 +0900

    init

---
+++ init.rb
@@ -0,0 +1,11 @@
+require 'redmine'
+require 'debeso_wikimacro'
+
+Redmine::Plugin.register :redmine_debeso_wiki_macro do
+  name 'Redmine Debeso Wiki Macro plugin'
+  author 'suer'
+  description 'Redmine Debeso Wiki Macro plugin'
+  version '0.0.1'
+  url 'http://github.com/suer/redmine_debeso_wiki_macro/'
+  author_url 'http://d.hatena.ne.jp/suer'
+end
